496cf81bca3c00d02f0103f7c13090dab813311f
Cheng-Hung Tsai <chenghung.tsai@email.ucr.edu>
2023-08-08 17:18:36 -0700

    Add new test for hmmsearch parser

--- tests/hmmsearch_parser_test.py
+++ tests/hmmsearch_parser_test.py
@@ -1,4 +1,5 @@
-from dbcanlight.hmmsearch_parser import overlap_filter
+from pathlib import Path
+from dbcanlight.hmmsearch_parser import hmmsearch_parser, overlap_filter
 
 
 def test_overlap_filter():
@@ -12,11 +13,19 @@ def test_overlap_filter():
             ["a.hmm", 100, "scof2", 1000, 2.23e-30, 1, 51, 301, 351, 0.50],
             ["b.hmm", 150, "scof1", 1000, 1.25e-33, 11, 21, 281, 381, 0.67],
             ["b.hmm", 150, "scof1", 1000, 1.11e-33, 11, 21, 271, 372, 0.68],
-        ]
+        ],
     }
     expect = [
         ["a.hmm", 100, "scof1", 500, "2.2e-30", 1, 51, 101, 151, "0.5"],
         ["a.hmm", 100, "scof1", 500, "2.2e-30", 1, 51, 126, 176, "0.5"],
         ["b.hmm", 150, "scof1", 1000, "1.1e-33", 11, 21, 271, 372, "0.68"],
-    ]   
+    ]
     assert overlap_filter(input) == expect
+
+
+def test_hmmsearch_parser():
+    input = "example/hmmsearch_output"
+    data_parser = hmmsearch_parser(Path(input))
+    results = data_parser.eval_cov_filter(1e-30, 0.35)
+    results = overlap_filter(results)
+    assert len(results) == 4 and len(results[0]) == 10
